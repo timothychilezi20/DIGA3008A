be61382af16533e99f350518a0974e628f92ed5b
timothychilezi20 <timothychilezi@gmail.com>
2025-05-10 21:29:00 +0200

    Practice

--- Blogs/Test/test.js
+++ Blogs/Test/test.js
@@ -1,13 +1,48 @@
 console.log("Dumb!"); 
 
-function exercise1(length, width){
- let length = 10; 
- let width = 5; 
+function exercise1(length, width)
+{
+  let width = 5; 
+  let length = 10
 
- let Area = length * width; 
- return Area;
+  let Area = length * width; 
+  return Area; 
 }
 
-function exercise2(){
-  
+function exercise2()
+{
+  let x = 5; 
+  let y = 5; 
+
+  if(x === y) {
+    return sum*3; 
+  }
+  else {
+    return sum
+  }
+}
+
+function exercise3()
+{
+ let randomNum1 = Math.floor(Math.random() * 51);
+ let randomNum2 = Math.floor(Math.random() * 51); 
+
+ if(randomNum1 === 50 || randomNum2 === 50 || (randomNum1+randomNum2) === 50)
+ {
+   return true; 
+ }
+ else 
+ {
+  return false; 
+ }
+}
+
+function exercise4(voltage, current)
+{
+ return current * voltage; 
+}
+
+function exercise5()
+{
+  let num1 = Math.floor()
 }
