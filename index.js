943470134b7a0e92ce5196d22c92584bf6e64d77
timothychilezi20 <timothychilezi@gmail.com>
2025-05-10 18:33:55 +0200

    Heading to JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+var num1 = 50; 
+
+let sentence = "My name is Timothy!"; 
+
+const constant = false; 
+constant = true; 
+console.log(constant); 
+
+//Object literal syntax
+let car = {speed: 300, model: "Dodge"}; 
+console.log(car.model); 
+
+randomNumber = Math.random(); 
+console.log(randomNumber); 
+
+//DOM Object
+window.alert("Hello I am JavaScript"); 
+document.write(<h1>Hello, I come from JavaScript!</h1>); 
