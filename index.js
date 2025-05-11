601ec675d72c4f4ebf77bb1e081a761840b7c8e6
timothychilezi20 <timothychilezi@gmail.com>
2025-05-11 21:15:26 +0200

    Fixed, I hope

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalId = null; 
 
-initializeSlider();
+//initializeSlider();
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
 function initializeSlider(){
   slides[slideIndex].classList.add("displaySlide");
+  intervalId = setInterval(nextSlide, 5000);
+  console.log(intervalId);
 }
 function showSlide(index){
 
