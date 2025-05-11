4b948d43ecddfc252ccbd16ceddace3d0202fc02
timothychilezi20 <timothychilezi@gmail.com>
2025-05-11 21:07:47 +0200

    Fixed, hopefully

---
+++ index.js
@@ -0,0 +1,20 @@
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null; 
+
+initializeSlider();
+
+function initializeSlider(){
+  slides[slideIndex].classList.add("displaySlide");
+}
+function showSlide(index){
+
+}
+function prevSlide(){
+
+}
+function nextSlide(){
+
+}
