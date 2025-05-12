1c3b25b0d2b3815e8dc23b7454f9e626f18e4a56
timothychilezi20 <timothychilezi@gmail.com>
2025-05-12 11:47:21 +0200

    Removed Slide

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 
 const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
-let intervalId = null; 
+let intervalId = null;
 
 //initializeSlider();
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
 function initializeSlider(){
+    
   slides[slideIndex].classList.add("displaySlide");
   intervalId = setInterval(nextSlide, 5000);
   console.log(intervalId);
