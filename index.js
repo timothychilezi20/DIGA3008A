fec4ab97a2af53ef22a46ae42771a1ee028890af
timothychilezi20 <timothychilezi@gmail.com>
2025-05-22 01:35:02 +0200

    Added Slider

---
+++ index.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const goToPage = {
+        GoToBlogs: "Blogs/BlogHomePage.html",
+        GoToProfile: "Profile/AboutMePage.html",
+        GoToDesign: "DesignSection/DesignHomepage.html",
+        GoToEssay: "Essays/EssaysHomepage.html",
+        GoToPortfolio: "Portfolio/Portfolio.html"
+    }
+
+    Object.keys(goToPage).forEach(buttonId => {
+        const button = document.getElementById(buttonId);
+        if (button) {
+            button.addEventListener("click", () => {
+                window.location.href = goToPage[buttonId];
+            });
+        }
+    });
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length; 
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+       slide.classList.remove('active');
+       if (i === index) slide.classList.add('active');
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide(){
+    currentSide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlides(currentSlide);
+}
+
+const nextBtn = document.querySelector('.nextSlide.next');
+const prevBtn = document.querySelector('.previousSlide.prev');
+
+if (nextBtn && prevBtn){
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', prevSlide);
+}
+
+setInterval(nextSlide, 5000);
+});
