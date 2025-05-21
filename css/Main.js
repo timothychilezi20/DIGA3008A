55777e7ade984a805b5f55a488e40a0a90a92882
timothychilezi20 <timothychilezi@gmail.com>
2025-05-21 02:29:10 +0200

    Fixed main css

--- css/Main.js
+++ css/Main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
     window.addEventListener("scroll", () => {
-        // Show button after scrolling down 300px
+       
         if (window.scrollY > 300) {
             scrollToTopBtn.style.display = "block";
         } else {
