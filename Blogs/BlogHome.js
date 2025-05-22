7c4d948fca2e2262c05b04add7482befd7a67d7c
timothychilezi20 <timothychilezi@gmail.com>
2025-05-22 18:35:44 +0200

    Fixed Button Links

--- Blogs/BlogHome.js
+++ Blogs/BlogHome.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const goToPage = {
         Week1: "AcademicWriting/Week1.html",
-        Week2: "Blogs/AcademicWriting/Week2.html",
-        Week3: "Blogs/AcademicWriting/Week3.html",
-        Week4: "Blogs/AcademicWriting/Week4.html",
-        Week5: "Blogs/AcademicWriting/Week5.html",
-        Week6: "Blogs/AcademicWriting/Week6.html",
-        Week7: "Blogs/AcademicWriting/Week7.html",
-        Week8: "Blogs/AcademicWriting/Week8.html",
-        Week9: "Blogs/AcademicWriting/Week9.html",
-        Week10: "Blogs/AcademicWriting/Week10.html",
-        Week11: "Blogs/AcademicWriting/Week11.html",
+        Week2: "AcademicWriting/Week2.html",
+        Week3: "AcademicWriting/Week3.html",
+        Week4: "AcademicWriting/Week4.html",
+        Week5: "AcademicWriting/Week5.html",
+        Week6: "AcademicWriting/Week6.html",
+        Week7: "AcademicWriting/Week7.html",
+        Week8: "AcademicWriting/Week8.html",
+        Week9: "AcademicWriting/Week9.html",
+        Week10: "AcademicWriting/Week10.html",
+        Week11: "AcademicWriting/Week11.html",
     }
 
     Object.keys(goToPage).forEach(buttonId => {
