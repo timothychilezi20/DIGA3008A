544e8bf4c92e37f94cdc3aea0b428b6a5fb31607
timothychilezi20 <timothychilezi@gmail.com>
2025-06-12 23:42:53 +0200

    Fixed JavaScript

--- Portfolio-Page/Script/Portfolio.js
+++ Portfolio-Page/Script/Portfolio.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const goToPage = {
-        Drawings: "DrawingsPortfolio.html",
-        Writing: "WritingPorfolio.html",
-        Games: "GamesPortfolio.html",
-        Assets: "AssetsPortfolio.html",
-    }
+  const goToPage = {
+    Drawings: "../HTML/DrawingsPortfolio.html",
+    Writing: "../HTML/WritingPorfolio.html",
+    Games: "../HTML/GamesPortfolio.html",
+    Assets: "../HTML/AssetsPortfolio.html",
+  };
 
-    Object.keys(goToPage).forEach(buttonId => {
-        const button = document.getElementById(buttonId);
-        if (button) {
-            button.addEventListener("click", () => {
-                window.location.href = goToPage[buttonId];
-            });
-        }
-    });
+  Object.keys(goToPage).forEach((buttonId) => {
+    const button = document.getElementById(buttonId);
+    if (button) {
+      button.addEventListener("click", () => {
+        window.location.href = goToPage[buttonId];
+      });
+    }
+  });
 });
 
 function goBack() {
-    window.history.back();
-  }
+  window.history.back();
+}
