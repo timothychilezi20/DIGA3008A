0d7494b82442b5f37b06b5d0819f06c0348d16ab
timothychilezi20 <timothychilezi@gmail.com>
2025-06-12 21:00:50 +0200

    updated files

--- css/Main.js
+++ css/Main.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-    window.addEventListener("scroll", () => {
-       
-        if (window.scrollY > 300) {
-            scrollToTopBtn.style.display = "block";
-        } else {
-            scrollToTopBtn.style.display = "none";
-        }
-    });
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
 
-    scrollToTopBtn.addEventListener("click", () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
+  });
 });
 
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll(".slide");
 
-slides.forEach(slide => {
-    const h2 = slide.querySelector('h2');
-    h2.style.color = '#fff';
-    h2.style.textShadow = '2px 2px 10px rgba(0, 0, 0, 0.8)';
+slides.forEach((slide) => {
+  const h2 = slide.querySelector("h2");
+  h2.style.color = "#fff";
+  h2.style.textShadow = "2px 2px 10px rgba(0, 0, 0, 0.8)";
 });
 
+function closeMenu() {
+  document.getElementById("check").checked = false;
+}
